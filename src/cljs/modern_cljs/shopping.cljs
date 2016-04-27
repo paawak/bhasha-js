c04ed665a225f3a5637a0860d0db5fdedaef2bc9
Palash Ray <paawak@gmail.com>
2016-04-26 22:31:47 -0400

    cleanup 1

--- src/cljs/modern_cljs/shopping.cljs
+++ src/cljs/modern_cljs/shopping.cljs
@@ -2,17 +2,14 @@
   (:require [domina.core :refer [by-id value set-value!]]
             [domina.events :refer [listen!]]))
 
-(defn calculate []
-  (let [quantity (value (by-id "quantity"))
-        price (value (by-id "price"))
-        tax (value (by-id "tax"))
-        discount (value (by-id "discount"))]
-    (set-value! (by-id "total") (-> (* quantity price)
-                                    (* (+ 1 (/ tax 100)))
-                                    (- discount)
-                                    (.toFixed 2)))))
+(defn setText []
+  (let [
+		text (value (by-id "bhashaTextArea"))
+       ]
+    (set-value! (by-id "bhashaTextArea") (-> (+ text '--monkey--')
+                                    ))))
 
 (defn ^:export init []
   (if (and js/document
            (.-getElementById js/document))
-    (listen! (by-id "calc") :click calculate)))
+    (listen! (by-id "bhashaTextArea") :click setText)))
