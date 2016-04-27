e700df2ba4f645159ff9ab6af722bde5db670c4e
Palash Ray <paawak@gmail.com>
2016-04-26 23:50:26 -0400

    printing keyChar, keyCode

--- src/cljs/bhasha/typing.cljs
+++ src/cljs/bhasha/typing.cljs
@@ -1,15 +1,33 @@
 (ns bhasha.typing
   (:require [domina.core :refer [by-id value set-value!]]
             [domina.events :refer [listen!]]))
+			
+(enable-console-print!)			
 
-(defn setText []
-  (let [
+(defn setText [evt]
+  (  
+  let [
 		text (value (by-id "bhashaTextArea"))
+		keyChar (:charCode evt)
+		keyCode (:keyCode evt)
        ]
-    (set-value! (by-id "bhashaTextArea") (-> (+ text '--monkey--')
-                                    ))))
+	   (comment
+			set-value! (by-id "bhashaTextArea") 
+			( 
+				-> (+ text '--monkey--')
+            )
+		)
+		(println "bhasha textArea keyPressed! keyChar: " keyChar ", keyCode: " keyCode)
+
+	)
+	
+	
+)
 
 (defn ^:export init []
-  (if (and js/document
-           (.-getElementById js/document))
-    (listen! (by-id "bhashaTextArea") :click setText)))
+	(if (
+			and js/document (.-getElementById js/document)
+		)
+		(listen! (by-id "bhashaTextArea") :keypress setText)
+	)
+)
