0062e90ea95b8a28ce5613f4c0aec1947e38190e
Palash Ray <paawak@gmail.com>
2016-04-30 14:55:24 -0400

    trying to simulate key press, not working

--- js/indic_editor.js
+++ js/indic_editor.js
@@ -1,9 +1,19 @@
 
 
 $(document).ready(function(){
-$('#indicTextEditor').bind('input propertychange', function() {
+$('#indicTextEditor').on('keydown keyup keypress', function(keyEvent) {
 
-      alert("hhh");
+      console.log("keyCode: "+keyEvent.keyCode + ", charCode: " + keyEvent.charCode + ", which:" + keyEvent.which);
+	  
+	  if (keyEvent.charCode == 97) {
+			//keyEvent.preventDefault();
+			var e1 = jQuery.Event( "keydown", {keyCode: 0, charCode: 13, which: 13} );			
+			$('#indicTextEditor').trigger(e1);
+			var e2 = jQuery.Event( "keypress", {keyCode: 0, charCode: 13, which: 13} );			
+			$('#indicTextEditor').trigger(e2);
+			var e3 = jQuery.Event( "keyup", {keyCode: 0, charCode: 13, which: 13} );			
+			$('#indicTextEditor').trigger(e3);
+	  }
 });
 });
 
