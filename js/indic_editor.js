24b12d5ef3ded06c6fda754b52c29bd4382b746e
Palash Ray <paawak@gmail.com>
2016-05-03 07:59:41 -0400

    jquery preventing user entering text on keypress

--- js/indic_editor.js
+++ js/indic_editor.js
@@ -1,13 +1,16 @@
 
 
 $(document).ready(function(){
+	
+
+	
 $('#indicTextEditor').keypress(function(keyEvent) {
 
       console.log("keyCode: "+keyEvent.keyCode + ", charCode: " + keyEvent.charCode + ", which:" + keyEvent.which);
 	  
 	  if (keyEvent.charCode == 97) {
 		  
-			//keyEvent.preventDefault();
+			keyEvent.preventDefault();
 			var code = 65;
 			keyEvent.which = code;
 			var e1 = jQuery.Event( "keydown", {which: code} );			
@@ -17,6 +20,8 @@ $('#indicTextEditor').keypress(function(keyEvent) {
 			var e3 = jQuery.Event( "keyup", {which: code} );			
 			$('#indicTextEditor').trigger(e3);
 	  }
+	  
+
 });
 });
 
